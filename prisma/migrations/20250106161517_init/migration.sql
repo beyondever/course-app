a75bf5d56553e5099ffe2d528108d6af785393ec
Thomas Andersen <thomas.andersen0523@gmail.com>
2025-01-06 17:16:34 +0100

    wip: migration based on the Prisma schema

---
+++ prisma/migrations/20250106161517_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "difficulty" TEXT NOT NULL
+);
